ea9fac6527aa854d995ca0e3ea27c3911895f88a
Dark-39 <jayminbilimoria39@gmail.com>
2025-06-26 00:28:26 +0530

    sloved errors in code loading

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: './', // ✅ THIS is the fix
+  build: {
+    outDir: '../dist', // ✅ build goes to root 'dist'
+    emptyOutDir: true,
+  },
 })
